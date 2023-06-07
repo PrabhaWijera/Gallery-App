9b4d6a03e75bae42ac955b9f2bfd557a48db74a4
Prabhash Wijerathna <106425954+PrabhaWijera@users.noreply.github.com>
2023-06-07 23:17:17 +0530

    upload2

--- app/src/main/java/com/example/gallerry_app/MainActivity.kt
+++ app/src/main/java/com/example/gallerry_app/MainActivity.kt
@@ -4,17 +4,20 @@ import android.content.Context
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ProgressBar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 import java.util.jar.Manifest
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     private var imageRecycler:RecyclerView?=null
     private var progressBar:ProgressBar?=null
-
+    private var allPicture:ArrayList<Image>?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +30,19 @@ class MainActivity : AppCompatActivity() {
         //storage permision
         if (ContextCompat.checkSelfPermission(this@MainActivity,android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),101)
+        }
+
+
+
+        allPicture= ArrayList()
+        if (allPicture!!.isEmpty()){
+
+            progressBar?.visibility=View.VISIBLE
+            // get all images from storage
+            allPicture=getAllImages()
+            //set Adapter to recycler
+            imageRecycler?.adapter=ImageAdapter(this,allPicture)
+            progressBar?.visibility=View.GONE
         }
     }
 }
